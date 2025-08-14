afd342ef5feb6041d5b1715727d4ec84e342485b
Omkar1972 <omkarbagmare49@gmail.com>
2025-08-14 18:01:17 +0530

    First commit

---
+++ AppendElement.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class AppendElement {
+
+	public static void main(String[] args) {
+	
+		HashSet<String> hs=new HashSet<String>();
+		
+		hs.add("Red");
+		hs.add("white");
+		hs.add("Pink");
+		hs.add("Yellow");
+		hs.add("Black");
+		hs.add("Green");
+		
+		System.out.println("The Hash Set: "+hs);
+
+	}
+
+}
+//Sample Output:The Hash Set: [Red, White, Pink, Yellow, Black, Green]
